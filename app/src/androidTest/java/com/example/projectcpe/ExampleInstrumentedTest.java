8a18f64fa75d4219ee3eb5961483cf2953418bf7
Taloengrat <armaerkarm@gmail.com>
2019-12-12 20:34:04 +0700

    version 5

--- app/src/androidTest/java/com/example/projectcpe/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/projectcpe/ExampleInstrumentedTest.java
@@ -20,6 +20,7 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
+        //
 
         assertEquals("com.example.projectcpe", appContext.getPackageName());
     }
